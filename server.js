c4b3393fe7d0b9e9da8fe57f38e7ee5ed9dd63bd
SebastianDarie <s.d.01.2002@gmail.com>
2020-09-29 18:42:05 +0300

    ex 3.7-3.8

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
+app.use(
+	morgan(
+		':method :url :status :res[content-length] - :response-time ms :body'
+	)
+)
 
 let persons = [
 	{
